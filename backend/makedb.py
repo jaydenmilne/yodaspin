cce73aa3b8470557ced4e1313ba692b1a46512df
Jayden Milne <jaydenmilne@users.noreply.github.com>
2020-11-12 19:43:29 -0700

    Add empty data

--- backend/makedb.py
+++ backend/makedb.py
@@ -6,7 +6,8 @@ print(f"Making db at {db_path}")
 db = sqlite3.connect(db_path)
 cur = db.cursor()
 
-cur.executescript("""
+cur.executescript(
+    """
 CREATE TABLE "highscores" (
 	"id"	TEXT NOT NULL CHECK(length("id") == 36) UNIQUE,
 	"name"	TEXT NOT NULL CHECK(length("name") <= 20 AND length("name") >= 2),
@@ -20,6 +21,16 @@ CREATE TABLE "minimum" (
 CREATE INDEX "spinsdex" ON "highscores" (
 	"spins"	DESC
 );
-""")
+"""
+)
+
+# things don't handle having no top score well
+cur.executescript(
+    """
+INSERT INTO highscores (id, name, spins, last_update) VALUES ("474aff56-761d-40bb-959e-fc87630dfdc0", "roguenerd", 1, 1);
+INSERT INTO minimum (min) VALUES (1);
+"""
+)
+
 cur.close()
 db.commit()
