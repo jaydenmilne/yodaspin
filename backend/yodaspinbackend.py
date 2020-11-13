a9a19c11b0a2c1c3647e8c17b89b9095d4df8c42
Jayden Milne <jaydenmilne@users.noreply.github.com>
2020-11-12 22:17:44 -0700

    Bump rate limiting

--- backend/yodaspinbackend.py
+++ backend/yodaspinbackend.py
@@ -254,7 +254,7 @@ def update():
 
 @app.route(f"/v{VERSION}/updateleaderboard", methods=["POST"])
 @limiter.limit(
-    "1/minute"
+    "2/minute"
 )  # assuming that there will only be one person with a highscore per ip address
 def updateleaderboard():
     response = update()
