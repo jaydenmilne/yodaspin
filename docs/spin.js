c9bd40d99cdc1e98e0ddfd1e9a5867eb8eccc2d4
Jayden Milne <jaydenmilne@users.noreply.github.com>
2021-04-27 08:57:03 -0600

    Season 3 release date

--- docs/spin.js
+++ docs/spin.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const DATES_OF_INTEREST = [
-    new Date(Date.UTC(2020, 9, 30, 7, 0, 0, 0)),
+    /*new Date(Date.UTC(2020, 9, 30, 7, 0, 0, 0)),
     new Date(Date.UTC(2020, 10, 6, 7, 0, 0, 0)),
     new Date(Date.UTC(2020, 10, 13, 8, 1, 0, 0)),
     new Date(Date.UTC(2020, 10, 20, 8, 1, 0, 0)),
@@ -9,6 +9,9 @@ const DATES_OF_INTEREST = [
     new Date(Date.UTC(2020, 11, 4, 8, 1, 0, 0)),
     new Date(Date.UTC(2020, 11, 11, 8, 1, 0, 0)),
     new Date(Date.UTC(2020, 11, 18, 8, 1, 0, 0))
+    */
+   // the release date is "christmas 2021" for now
+   new Date(Date.UTC(2021, 12, 25, 8, 0, 0, 0))
 ]
 
 const SONGS = [
